b52d42fb2d32be72acc7d8a1fe34faccc5cda4a0
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2022-03-23 02:58:56 +0530

    about page

--- about.php
+++ about.php
@@ -17,7 +17,7 @@ include "includes/head.php"
           <div class="col-lg-7 mx-auto align-self-center">
             <div class=" text-center">
               <h1>About Us</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati natus iure voluptatum eveniet harum recusandae ducimus saepe.</p>ˀ
+
             </div>
           </div>
         </div>
@@ -31,7 +31,6 @@ include "includes/head.php"
             <div class="block-16">
               <figure>
                 <img src="images/bg_1.jpg" alt="Image placeholder" class="img-fluid rounded">
-                <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="icon-play"></span></a>
 
               </figure>
             </div>
@@ -41,13 +40,13 @@ include "includes/head.php"
 
 
             <div class="site-section-heading pt-3 mb-4">
-              <h2 class="text-black">How We Started</h2>
+              <h2 class="text-black">WHO WE ARE</h2>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius repellat, dicta at laboriosam, nemo
-              exercitationem itaque eveniet architecto cumque, deleniti commodi molestias repellendus quos sequi hic fugiat
-              asperiores illum. Atque, in, fuga excepturi corrupti error corporis aliquam unde nostrum quas.</p>
-            <p>Accusantium dolor ratione maiores est deleniti nihil? Dignissimos est, sunt nulla illum autem in, quibusdam
-              cumque recusandae, laudantium minima repellendus.</p>
+            <p>Pharma.com, is brought to you by the Pharma Company – one of India’s
+              most trusted pharmacies, with over 100 years’ experience in dispensing quality medicines.
+              At netmeds.com, we help you look after your own health effortlessly as well as take care
+              of loved ones wherever they may reside in India. You can buy and send medicines from any
+              corner of the country - with just a few clicks of the mouse.</p>
 
           </div>
         </div>
@@ -63,8 +62,6 @@ include "includes/head.php"
             <div class="block-16">
               <figure>
                 <img src="images/hero_1.jpg" alt="Image placeholder" class="img-fluid rounded">
-                <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="icon-play"></span></a>
-
               </figure>
             </div>
           </div>
@@ -72,13 +69,13 @@ include "includes/head.php"
 
 
             <div class="site-section-heading pt-3 mb-4">
-              <h2 class="text-black">We Are Trusted Company</h2>
+              <h2 class="text-black">ONE-STOP SHOP</h2>
             </div>
-            <p class="text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius repellat, dicta at laboriosam, nemo
-              exercitationem itaque eveniet architecto cumque, deleniti commodi molestias repellendus quos sequi hic fugiat
-              asperiores illum. Atque, in, fuga excepturi corrupti error corporis aliquam unde nostrum quas.</p>
-            <p class="text-black">Accusantium dolor ratione maiores est deleniti nihil? Dignissimos est, sunt nulla illum autem in, quibusdam
-              cumque recusandae, laudantium minima repellendus.</p>
+            <p class="text-black">Pharma.com continues a legacy of 100 years of success in the
+              pharmaceutical industry. We are committed to provide safe, reliable and affordable medicines
+              as well as a customer service philosophy that is worthy of our valued customers’ loyalty.
+              We offer a superior online shopping experience, which includes ease of navigation and
+              absolute transactional security.</p>
 
           </div>
         </div>
@@ -94,8 +91,8 @@ include "includes/head.php"
             </div>
             <div class="text">
               <h2>Free Shipping</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan
-                tincidunt fringilla.</p>
+              <p>FREE Shipping if you
+                Order ₹200 or more </p>
             </div>
           </div>
           <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="100">
@@ -104,8 +101,7 @@ include "includes/head.php"
             </div>
             <div class="text">
               <h2>Free Returns</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan
-                tincidunt fringilla.</p>
+              <p>You can return any product within 48h with no charges</p>
             </div>
           </div>
           <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="200">
@@ -114,8 +110,7 @@ include "includes/head.php"
             </div>
             <div class="text">
               <h2>Customer Support</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan
-                tincidunt fringilla.</p>
+              <p>24/7 customer support to ensure the customers satisfaction</p>
             </div>
           </div>
         </div>
@@ -123,78 +118,7 @@ include "includes/head.php"
     </div>
 
 
-    <div class="site-section bg-light custom-border-bottom" data-aos="fade">
-      <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7 site-section-heading text-center pt-4">
-            <h2>The Team</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 col-lg-6 mb-5">
 
-            <div class="block-38 text-center">
-              <div class="block-38-img">
-                <div class="block-38-header">
-                  <img src="images/person_1.jpg" alt="Image placeholder" class="mb-4">
-                  <h3 class="block-38-heading h4">Elizabeth Graham</h3>
-                  <p class="block-38-subheading">CEO/Co-Founder</p>
-                </div>
-                <div class="block-38-body">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
-                    recusandae doloribus ut fugit officia voluptate soluta. </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-6 mb-5">
-            <div class="block-38 text-center">
-              <div class="block-38-img">
-                <div class="block-38-header">
-                  <img src="images/person_2.jpg" alt="Image placeholder" class="mb-4">
-                  <h3 class="block-38-heading h4">Jennifer Greive</h3>
-                  <p class="block-38-subheading">Co-Founder</p>
-                </div>
-                <div class="block-38-body">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
-                    recusandae doloribus ut fugit officia voluptate soluta. </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-6 mb-5">
-            <div class="block-38 text-center">
-              <div class="block-38-img">
-                <div class="block-38-header">
-                  <img src="images/person_3.jpg" alt="Image placeholder" class="mb-4">
-                  <h3 class="block-38-heading h4">Patrick Marx</h3>
-                  <p class="block-38-subheading">Marketing</p>
-                </div>
-                <div class="block-38-body">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
-                    recusandae doloribus ut fugit officia voluptate soluta. </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-6 mb-5">
-            <div class="block-38 text-center">
-              <div class="block-38-img">
-                <div class="block-38-header">
-                  <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4">
-                  <h3 class="block-38-heading h4">Mike Coolbert</h3>
-                  <p class="block-38-subheading">Sales Manager</p>
-                </div>
-                <div class="block-38-body">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
-                    recusandae doloribus ut fugit officia voluptate soluta. </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
 
     <?php
