37e38c4cb7b71d532ad14e20938c0716b1f479dd
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2024-07-29 12:53:00 +0530

    change the positioning of boxes

--- admin/index.php
+++ admin/index.php
@@ -1,59 +1,51 @@
-<?php
-include "includes/head.php";
-?>
+<?php include "includes/head.php"; ?>
 
 <body>
 
-  <?php
-  include "includes/header.php";
-  ?>
+  <?php include "includes/header.php"; ?>
 
-  <div class=" container-fluid">
+  <div class="container-fluid">
 
-    <?php
-    include "includes/sidebar.php";
-    ?>
+    <?php include "includes/sidebar.php"; ?>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <br>
-      <div class="row">
-        <div class="card" style="width: 25rem;margin-bottom: 20px ;margin-right: 200px ;">
+      <div class="d-flex justify-content-center flex-wrap">
+        <div class="card m-4" style="width: 25rem;">
           <a href="orders.php">
-            <img class="card-img-top" src="../images/shopping-cart.svg" alt="Card image cap" style="width: 5rem;margin-top: 20px ;">
+            <img class="card-img-top mx-auto d-block" src="../images/shopping-cart.svg" alt="Card image cap" style="width: 5rem; margin-top: 20px;">
           </a>
-          <div class="card-body">
+          <div class="card-body text-center">
             <h5 class="card-title">Orders</h5>
             <p class="card-text">Display and modify the orders details.</p>
             <a href="orders.php" class="btn btn-primary">Go to orders page</a>
           </div>
         </div>
-        <div class="card" style="width: 25rem;margin-bottom: 20px ;">
+        <div class="card m-4" style="width: 25rem;">
           <a href="products.php">
-            <img class="card-img-top" src="../images/package.svg" alt="Card image cap" style="width: 5rem;margin-top: 20px ;">
+            <img class="card-img-top mx-auto d-block" src="../images/package.svg" alt="Card image cap" style="width: 5rem; margin-top: 20px;">
           </a>
-          <div class="card-body">
+          <div class="card-body text-center">
             <h5 class="card-title">Products</h5>
             <p class="card-text">Display and modify the products details.</p>
             <a href="products.php" class="btn btn-primary">Go to products page</a>
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="card" style="width: 25rem;margin-top: 20px ;margin-right: 200px ;">
+        <div class="card m-4" style="width: 25rem;">
           <a href="customers.php">
-            <img class="card-img-top" src="../images/users.svg" alt="Card image cap" style="width: 5rem;margin-top: 20px ;">
+            <img class="card-img-top mx-auto d-block" src="../images/users.svg" alt="Card image cap" style="width: 5rem; margin-top: 20px;">
           </a>
-          <div class="card-body">
+          <div class="card-body text-center">
             <h5 class="card-title">Customers</h5>
             <p class="card-text">Display and modify the customers details.</p>
             <a href="customers.php" class="btn btn-primary">Go to customers page</a>
           </div>
         </div>
-        <div class="card" style="width: 25rem;margin-top: 20px ;">
+        <div class="card m-4" style="width: 25rem;">
           <a href="admin.php">
-            <img class="card-img-top" src="../images/user.svg" alt="Card image cap" style="width: 5rem;margin-top: 20px ;">
+            <img class="card-img-top mx-auto d-block" src="../images/user.svg" alt="Card image cap" style="width: 5rem; margin-top: 20px;">
           </a>
-          <div class="card-body">
+          <div class="card-body text-center">
             <h5 class="card-title">Admin</h5>
             <p class="card-text">Display and modify the admins details.</p>
             <a href="admin.php" class="btn btn-primary">Go to admin page</a>
@@ -63,9 +55,7 @@ include "includes/head.php";
     </main>
   </div>
 
-  <?php
-  include "includes/footer.php"
-  ?>
+  <?php include "includes/footer.php"; ?>
 </body>
 
 </html>
